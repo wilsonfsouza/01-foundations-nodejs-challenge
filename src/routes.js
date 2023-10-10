e21b09e6022df3bfc0500ed55f2fe96886822d1d
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-09 21:09:04 -0700

    feat: added route to list all tasks and allows searching by title and/or description

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database()
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { search } = req.query
+
+      const tasks = database.select(
+        'tasks',
+        search
+          ? {
+              title: search,
+              description: search,
+            }
+          : null,
+      )
+
+      return res.end(JSON.stringify(tasks))
+    },
+  },
+]
