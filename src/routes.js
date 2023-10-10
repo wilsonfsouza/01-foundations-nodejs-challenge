d363847c4a225070a98d107435f0005e3c9537f2
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-09 21:22:26 -0700

    feat: added POST route to create a task validating title and description as required fields

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import { randomUUID } from 'node:crypto'
 import { Database } from './database.js'
 import { buildRoutePath } from './utils/build-route-path.js'
 
@@ -21,6 +22,38 @@ export const routes = [
       )
 
       return res.end(JSON.stringify(tasks))
+    },
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { title, description } = req.body
+
+      if (!title) {
+        return res
+          .writeHead(400)
+          .end(JSON.stringify({ message: 'The field title is required' }))
+      }
+
+      if (!description) {
+        return res
+          .writeHead(400)
+          .end(JSON.stringify({ message: 'The field description is required' }))
+      }
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date(),
+      }
+
+      database.insert('tasks', task)
+
+      return res.writeHead(201).end()
     },
   },
 ]
