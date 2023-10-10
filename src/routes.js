f65ca48ac7b8ce5cda5bd47dde7d8481635f8bd1
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-10 07:32:30 -0700

    feat: added PUT route to update tash by id

--- src/routes.js
+++ src/routes.js
@@ -67,4 +67,30 @@ export const routes = [
       return res.writeHead(204).end()
     },
   },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { title, description } = req.body
+
+      if (!title || !description) {
+        return res.writeHead(400).end(
+          JSON.stringify({
+            message: 'Both title and description are required',
+          }),
+        )
+      }
+
+      const task = {
+        title,
+        description,
+        updated_at: new Date(),
+      }
+
+      database.update('tasks', id, task)
+
+      return res.writeHead(204).end()
+    },
+  },
 ]
