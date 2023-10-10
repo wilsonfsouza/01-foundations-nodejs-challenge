f5a03efbb8ff6e43230182d36bb8d84aba7b6032
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-10 07:16:51 -0700

    feat: added DELETE route to remove a task by id

--- src/routes.js
+++ src/routes.js
@@ -56,4 +56,15 @@ export const routes = [
       return res.writeHead(201).end()
     },
   },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      database.delete('tasks', id)
+
+      return res.writeHead(204).end()
+    },
+  },
 ]
